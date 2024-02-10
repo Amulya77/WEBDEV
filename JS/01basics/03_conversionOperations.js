c3afa75f8ce778da5e17eb6a289449d58a230ac4
Amulya Kumar Maurya <79034752+Amulya77@users.noreply.github.com>
2024-02-11 00:23:27 +0530

    Add files via upload
    Learned conversion of variables

---
+++ JS/01basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = '100';
+let score2 = "AMUl100";
+let score3 = null;
+
+console.log(typeof(score)); //string
+console.log(score + 1); //1001
+console.log('Score: ' + score); // Score: 100
+
+let valueInNumber=Number(score);
+let valueInNumber2=Number(score2);
+console.log(typeof(valueInNumber))//number
+console.log(valueInNumber2);//NAN
+console.log(score3); 
